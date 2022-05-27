966bc20722ee9c1a3b52b13531cbce2abf1173ca
Ramshad <cpramshad@gmail.com>
2022-05-27 22:15:58 +0400

    add craco config. Add Button component and use it in app

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from '@cpramshad/components';
 
 function App() {
+  console.log(Button);
   const [count, setCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         count is {count}
-        <button onClick={() => {
+        <Button onClick={() => {
           setCount(prev => ++prev)
-        }}>Click me!</button>
+        }} />
       </header>
     </div>
   );
